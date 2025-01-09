778d8728c2c13e6afd5b1bcf7f75e673ecc15edd
irakli janashvili <iraklijanashvili1999@gmail.com>
2025-01-10 03:36:14 +0400

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Imac on 10.01.25.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
